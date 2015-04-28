0e9e58263589cb2140e7f92919df9dad5f703d4e
AdityaAthavale <adityaathavale@Adityas-MacBook-Pro.local>
2015-04-27 21:00:14 -0400

    Initial Commit

---
+++ LeviTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeviTest2
+//
+//  Created by Aditya Athavale on 4/27/15.
+//  Copyright (c) 2015 Aditya Athavale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
